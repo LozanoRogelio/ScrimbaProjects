3577ec406b4ef3f1ec26b86ea06c625e957c8605
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-03-30 15:23:09 -0500

    Added increment and decrements in likes and shares. Added visibility to comments and ability to toggle

--- Twimba/index.js
+++ Twimba/index.js
@@ -10,21 +10,53 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
     }
-    /*
-    Challenge:
-    1. If a like icon has been clicked, call handleLikeClick
-       passing in the uuid that is stored in the like icon's
-       data attribute.
-    */
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
 })
 
 function handleLikeClick(tweetId){
-    console.log(tweetId)
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
     /*
     Challenge:
-    2. handleLikeClick should take in a parameter.
-       You can call this parameter 'tweetId'. For
-       now just log out tweetId.
+    1. Use the uuid stored in 'replyId' to take control
+       of the div containing that tweet’s replies.
+       (Check the HTML string below to remind yourself
+       what id that div will have.)
+    2. Toggle the CSS class "hidden" on that div.
     */
 }
 
@@ -32,6 +64,38 @@ function getFeedHtml(){
     let feedHtml = ``
 
     tweetsData.forEach(function(tweet){
+
+        let likeIconClass = ''
+
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+
+        let retweetIconClass = ''
+
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+
+        let repliesHtml = ''
+
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
+        }
+
+
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -47,13 +111,13 @@ function getFeedHtml(){
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"
+                    <i class="fa-solid fa-heart ${likeIconClass}"
                     data-like="${tweet.uuid}"
                     ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
@@ -61,6 +125,9 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
 </div>
 `
     })
@@ -72,4 +139,3 @@ function render(){
 }
 
 render()
-
